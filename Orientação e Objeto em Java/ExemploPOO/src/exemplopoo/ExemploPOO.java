cd5ad625e82302abe686e516a96d75957a7fd07c
Leonardo Buso <leobuso.souza@gmail.com>
2025-05-12 09:30:50 -0300

    Terminar

--- Orientação e Objeto em Java/ExemploPOO/src/exemplopoo/ExemploPOO.java
+++ Orientação e Objeto em Java/ExemploPOO/src/exemplopoo/ExemploPOO.java
@@ -9,52 +9,77 @@ package exemplopoo;
  * @author Usuário
  */
 public class ExemploPOO {
-    
-    public static void main (String[] args){  // a main é necessária para executar o código e para que os dados possam corresponder aos seus devidos atributos.
-        Peças meu_carro = new Peças ("Nivus", "Cinza", "Volkswagen", "Carburador-xx", "Farol", "Esportivo");
-        meu_carro.print();
-    }  
-}
 // Classe Carro    }
-    class Carro {
+       public class Carro {
        private String modelo; // atributo de carro
        private String cor;  // atributo de carro
        private String marca;  // atributo de carro
-       
-       Carro(String modelo, String cor, String marca) {
-           this.modelo = modelo; // mostra o valor do atributo
-           this.cor = cor;
-           this.marca = marca;  
-       }
-       
-       public String getModelo() { // aceesa pelo get o valor  do medelo desejado e retorna
+      
+       public String getmodelo(){
            return modelo;
        }
        
-       public String getCor() {
+       public void setmodelo(String modelo){
+           this.modelo = modelo;
+       }
+       
+       public String getcor(){
            return cor;
        }
        
-       public String getMarca() {
+       public void setcor(String cor) {
+           this.cor = cor;
+       }
+       
+        public String getmarca(){
            return marca;
        }
        
+       public void setmarca(String marca) {
+           this.marca = marca;
+       }
+       
+       public void acelerar(){
+           System.out.println("O carro acelerou");  
+       }
+       public void freiar(){
+           System.out.println("O carro freiou");
+       }
     }
+
 // Classe Peças
-    class Peças extends Carro {
+        public class Peças extends Carro {
         private String carburador; // atributos da classe peças que é uma "filha" da classe carro
         private String lanterna;
         private String volante;
 
-        public Peças(String modelo, String cor, String marca, String carburador, String lanterna, String volante) {
-            super(modelo, cor, marca); // chama os valores da "mãe" para poder usá-los através do "super"
-            this.carburador = carburador; // valor dos atributos
-            this.lanterna = lanterna;
-            this.volante = volante;
-        }
+       public String getcarburador(){
+           return carburador;
+       }
+       
+       public void setcarburador(String carburador){
+           this.modelo = modelo;
+       }
+       
+       public String getcor(){
+           return cor;
+       }
+       
+       public void setcor(String cor) {
+           this.cor = cor;
+       }
+       
+        public String getmarca(){
+           return marca;
+       }
+       
+       public void setmarca(String marca) {
+           this.marca = marca;
+       }
         
         public void print() { // imprime todos os dados referentes aos seus devidos atributos
             System.out.println(getModelo() + " " + getCor() + " " + getMarca() + " " + carburador + " " + lanterna + " " + volante);
         }
     }
 
+}
---
+++ Orientação e Objeto em Java/ExemploPOO/src/exemplopoo/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemplopoo;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Main {
+        public static void main (String[] args){  // a main é necessária para executar o código e para que os dados possam corresponder aos seus devidos atributos.
+        Peças meuCarro = new Peças ("Nivus", "Cinza", "Volkswagen", "Carburador-xx", "Farol", "Esportivo");
+        meuCarro.print();
+    }  
+}
+
