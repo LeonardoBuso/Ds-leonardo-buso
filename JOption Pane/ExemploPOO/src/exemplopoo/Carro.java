0c905aec864968c42d062591508701b9b1841e99
Leonardo Buso <leobuso.souza@gmail.com>
2025-05-26 09:16:22 -0300

    Arquivos com JOptionPane

---
+++ JOption Pane/ExemploPOO/src/exemplopoo/Carro.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemplopoo;
+import javax.swing.JOptionPane;
+
+public class Carro {
+   // Classe Carro    
+       private String modelo; // atributo de carro
+       private String cor;  // atributo de carro
+       private String marca;  // atributo de carro
+       public String acelerar;
+       public String freiar;
+    public Carro(String modelo, String cor, String marca) {
+        this.modelo = modelo;
+        this.cor = cor;
+        this.marca = marca;
+    }
+
+       public String getmodelo(){
+           return modelo;
+       }
+       
+       public void setmodelo(String modelo){
+           this.modelo = modelo;
+       }
+       
+       public String getcor(){
+           return cor;
+       }
+       
+       public void setcor(String cor) {
+           this.cor = cor;
+       }
+       
+        public String getmarca(){
+           return marca;
+       }
+       
+       public void setmarca(String marca) {
+           this.marca = marca;
+       }
+       
+       
+    }
+
---
+++ JOption Pane/ExemploPOO/src/exemplopoo/Main.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemplopoo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Main {
+        public static void main (String[] args)  {  // a main é necessária para executar o código e para que os dados possam corresponder aos seus devidos atributos.
+      
+ 
+        String marca = JOptionPane.showInputDialog("Entre com a marca do carro");
+        String modelo = JOptionPane.showInputDialog("Entre com o modelo do carro");
+        String cor = JOptionPane.showInputDialog("Entre com a cor do carro");
+        String volante = JOptionPane.showInputDialog("Entre com o volante do carro");
+        String carburador = JOptionPane.showInputDialog("Entre com o carburador do carro");
+        String lanterna = JOptionPane.showInputDialog("Entre com a lanterna do carro");
+        
+        JOptionPane.showMessageDialog(null, "Carro " + marca + "");
+        JOptionPane.showMessageDialog(null, "Modelo " + modelo + "" );
+        JOptionPane.showMessageDialog(null, "Cor " + cor + "" );
+        JOptionPane.showMessageDialog(null, "Volante " + volante + "" );
+        JOptionPane.showMessageDialog(null, "Carburador " + carburador + "" );
+        JOptionPane.showMessageDialog(null, "Lanterna " + lanterna + "" );
+        
+        int escolha = JOptionPane.showConfirmDialog(null,"O carro acelerou? ", "Confirmado ", JOptionPane.YES_NO_OPTION);
+        
+        if (escolha == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "O carro acelerou");
+        } else {
+            JOptionPane.showMessageDialog(null, "O carro não acelerou");
+        }
+        
+        int escolha_dois = JOptionPane.showConfirmDialog(null,"O carro freiou? ", "Confirmado ", JOptionPane.YES_NO_OPTION);
+        
+        if (escolha_dois == JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "O carro freiou");
+        } else {
+            JOptionPane.showMessageDialog(null, "O carro não freiou");
+        }
+        
+               
+    }        
+        
+}
+
