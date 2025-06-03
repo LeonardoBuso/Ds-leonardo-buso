f4ab068b155e5fdea65bc80eca46fbfb9967cc60
LeonardoBuso <leobuso.souza@gmail.com>
2025-06-03 10:08:18 -0300

    Atualizar o Main.java

--- JOption Pane/ExemploPOO/src/exemplopoo/Main.java
+++ JOption Pane/ExemploPOO/src/exemplopoo/Main.java
@@ -14,14 +14,14 @@ public class Main {
         public static void main (String[] args)  {  // a main é necessária para executar o código e para que os dados possam corresponder aos seus devidos atributos.
       
  
-        String marca = JOptionPane.showInputDialog("Entre com a marca do carro");
+        String marca = JOptionPane.showInputDialog("Entre com a marca do carro"); // Entrada de dados 
         String modelo = JOptionPane.showInputDialog("Entre com o modelo do carro");
         String cor = JOptionPane.showInputDialog("Entre com a cor do carro");
         String volante = JOptionPane.showInputDialog("Entre com o volante do carro");
         String carburador = JOptionPane.showInputDialog("Entre com o carburador do carro");
         String lanterna = JOptionPane.showInputDialog("Entre com a lanterna do carro");
         
-        JOptionPane.showMessageDialog(null, "Carro " + marca + "");
+        JOptionPane.showMessageDialog(null, "Carro " + marca + ""); // exibe na tela
         JOptionPane.showMessageDialog(null, "Modelo " + modelo + "" );
         JOptionPane.showMessageDialog(null, "Cor " + cor + "" );
         JOptionPane.showMessageDialog(null, "Volante " + volante + "" );
@@ -30,7 +30,7 @@ public class Main {
         
         int escolha = JOptionPane.showConfirmDialog(null,"O carro acelerou? ", "Confirmado ", JOptionPane.YES_NO_OPTION);
         
-        if (escolha == JOptionPane.YES_OPTION) {
+        if (escolha == JOptionPane.YES_OPTION) { // condições 
             JOptionPane.showMessageDialog(null, "O carro acelerou");
         } else {
             JOptionPane.showMessageDialog(null, "O carro não acelerou");
