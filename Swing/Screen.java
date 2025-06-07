5f8f2f75b0bca50e20024f2986b7569702b82aba
Leonardo Buso <leobuso.souza@gmail.com>
2025-06-07 14:29:28 -0300

    Modificações na Interface

--- Swing/Screen.java
+++ Swing/Screen.java
@@ -7,18 +7,18 @@ public class Screen extends JFrame implements ActionListener {
     public Screen() {
 
         setVisible(true); //mostra na tela
-        setSize(800,500); //tamanho da tela
+        setSize(600,600); //tamanho da tela
         setTitle("Teste"); //título da tela
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //ao fechar a tela o código para automaticamente 
         setResizable(false); //impossibilita a modificação do tamanho da tela
         setLocationRelativeTo(null);
-        setLayout(null); // anula todos os valores 
+        setLayout(null); // anula todos os valores
 
-        JButton jButton = new JButton("If want yes"); 
-        jButton.setBounds(100,200,250,70); // tamanho
-        jButton.setFont(new Font("Arial", Font.PLAIN, 40)); 
-        jButton.setForeground(new Color(10,10,10)); // letra
-        jButton.setBackground(new Color(33, 82, 234)); //fundo
+        JButton jButton = new JButton("START");
+        jButton.setBounds(160,400,250,70); // tamanho
+        jButton.setFont(new Font("Arial", Font.BOLD , 40));
+        jButton.setForeground(new Color(63, 6, 121)); // letra
+        jButton.setBackground(new Color(91, 204, 136)); //fundo
 
         add(jButton);
 
